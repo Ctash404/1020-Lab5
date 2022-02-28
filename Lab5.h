8063966d8c48286a6225b294673af27b6a0c962e
ctash404 <ctash@g.clemson.edu>
2022-02-28 14:47:29 -0500

    hi

--- Lab5.h
+++ Lab5.h
@@ -6,18 +6,20 @@ using namespace std;
 class Student
 {
     private:
-        string name {"default"}, email {"default"}, 
-            address {"default"}, year {"default"};
-        double gpa {2.0};
+        string name {"\0"}, email {"\0"}, 
+            address {"\0"}, year {"\0"};
+        double gpa {0.0};
     public:
         Student() = default;
         Student(string n, string e, string a, string y, double g) :
         name{n}, email{e}, address{a}, year{y}, gpa{g} {}
+        
         bool setName(string n);
         bool setEmail(string e);
         bool setAddress(string a);
         bool setYear(string y);
         bool setGpa(double g);
+        
         string getName();
         string getEmail();
         string getAddress();
@@ -33,11 +35,13 @@ class Car
         Car() = default;
         Car(string ma, string mo, string y, string l, string t) :
         make{ma}, model{mo}, year{y}, lowEmission{l}, tinted{t} {}
+        
         bool setMake(string ma);
         bool setModel(string mo);
         bool setYear(string l);
         bool setLowEmission(string a);
         bool setTinted(string t);
+        
         string getModel();
         string getMake();
         string getYear();
