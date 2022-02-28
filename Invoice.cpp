6701c2d0ed32b9b85b73ec62c84c03896b3d7397
ctash404 <ctash@g.clemson.edu>
2022-02-28 15:20:55 -0500

    try3

--- Invoice.cpp
+++ Invoice.cpp
@@ -1,1 +1,45 @@
-#include
+#include "Invoice.h"
+
+bool setPermitPrice(string p)
+{
+    bool valid = false;
+
+    if (!p.empty())
+    {
+        valid = true;
+    }
+    return valid;
+}
+bool setDiscount(double d)
+{
+    bool valid = false;
+
+    if (d < 0.0)
+    {
+        valid = true;
+    }
+    return valid;
+}
+bool setServiceCharge(double s)
+{
+    bool valid = false;
+
+    if (s < 0.0)
+    {
+        valid = true;
+    }
+    return valid;
+}
+
+string getPermitPrice()
+{
+
+}
+double getDiscount()
+{
+
+}
+double getServiceCharfe()
+{
+
+}
